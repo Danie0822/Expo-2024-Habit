73dd8a2f49af59b92cba1308166670d0258b66d8
Danie0822 <20190652@ricaldone.edu.sv>
2024-04-05 07:49:50 -0600

    el menu incorporado desde js y tabla

---
+++ Controladores/Privada/cambio_mod.js
@@ -0,0 +1,18 @@
+/* funcion de cambio de modo  */
+function toggleMode() {
+    const body = document.body;
+    const isLightMode = body.classList.contains('light-mode');
+    if (isLightMode) {
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
+      /* Guarda el cambio en local storage  */
+      localStorage.setItem('mode', 'dark');
+    } else {
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
+      /* Guarda el cambio en local storage  */
+      localStorage.setItem('mode', 'light');
+    }
+  }
+  
+  
