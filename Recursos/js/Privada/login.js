866316c4847ff9c085edba12bab333f1733a3dd7
Danie0822 <20190652@ricaldone.edu.sv>
2024-04-06 21:19:05 -0600

    comentarios

--- Recursos/js/Privada/login.js
+++ Recursos/js/Privada/login.js
@@ -1,3 +1,5 @@
+/* funcionalidad de modo oscuros y claro   */
+
 document.addEventListener("DOMContentLoaded", function() {
     const body = document.body;
     const savedMode = localStorage.getItem('mode');
@@ -5,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (savedMode === 'dark') {
         body.classList.add('dark-mode');
+        /* cambio de imagen   */
         logoImage.src = '../../recursos/img/privada/logo-dark.png';
     } else {
         body.classList.add('light-mode');
+            /* cambio de imagen   */
         logoImage.src = '../../recursos/img/privada/logo.png';
     }
     
